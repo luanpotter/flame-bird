7826aa3a092a1f530ccfdebb0b416f5902e8aaca
Luan Nico <luanpotter27@gmail.com>
2020-01-11 15:10:07 -0500

    adding bg

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flame/components/animation_component.dart';
+import 'package:flame/components/component.dart';
+import 'package:flame/components/mixins/resizable.dart';
 import 'package:flame/flame.dart';
 import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
@@ -10,10 +12,22 @@ void main() async {
   runApp(game.widget);
 }
 
+class Bg extends Component with Resizable {
+  static final _blue = Paint()..color = const Color(0xFF5B6EE1);
+
+  @override
+  void render(Canvas c) {
+      c.drawRect(Rect.fromLTWH(0.0, 0.0, size.width, size.height), _blue);
+  }
+
+  @override
+  void update(double t) {}
+}
+
 class Bird extends AnimationComponent {
   static const SIZE = 32.0;
 
-  Bird(Size size) : super.sequenced(SIZE, SIZE, 'bird.png', 3) {
+  Bird(Size size) : super.sequenced(SIZE, SIZE, 'bird.png', 3, textureWidth: 8.0, textureHeight: 8.0) {
     this.x = (size.width - SIZE) / 2;
     this.y = (size.height - SIZE) / 2;
   }
@@ -21,6 +35,7 @@ class Bird extends AnimationComponent {
 
 class MyGame extends BaseGame {
   MyGame(Size size) {
+    add(Bg());
     add(Bird(size));
   }
 }
